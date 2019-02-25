b60f6b445424a38b349fb7399cf180b2116d704c
Simon Kiener <jugglinghobo@gmail.com>
2019-02-25 22:02:23 +0100

    added preferences to payment gateway

--- app/models/spree/payment_method/six_payment_page.rb
+++ app/models/spree/payment_method/six_payment_page.rb
@@ -4,5 +4,13 @@ module Spree
     def partial_name
       :sixpaymentpage
     end
+
+    preference :terminal_id, :string, default: '17925560'
+    preference :customer_id, :string, default: '245294'
+    preference :spec_version, :string, default: '1.10'
+    preference :payment_description, :string, default: "NILE Bestellung"
+    preference :success_url, :string, default: '/create_payment'
+    preference :fail_url, :string, default: '/fail_payment'
+
   end
 end
